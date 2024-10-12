d42bc98689566cab148d76ea562912ab0d69262f
lukehinojosa <lukehinojosa05@gmail.com>
2024-10-11 22:29:27 -0400

    Only jump when grounded

--- Assets/Scripts/BallMove.cs
+++ Assets/Scripts/BallMove.cs
@@ -8,33 +8,48 @@ public class BallMove : MonoBehaviour
     public float jumpSpeed = 1f;
 
     public Rigidbody rb;
+
+    public bool grounded;
     
     void Update()
     {
         RaycastHit rightHit;
         RaycastHit leftHit;
-        if (Physics.Raycast(transform.TransformPoint(Vector3.right / 2), Vector3.down, out rightHit, Mathf.Infinity))
+        if (Physics.Raycast(transform.TransformPoint(Vector3.right / 2), Vector3.down, out rightHit, transform.TransformPoint(Vector3.right / 2).y - transform.TransformPoint(Vector3.down / 2).y))
         {
             Debug.DrawLine(transform.TransformPoint(Vector3.right / 2), rightHit.point, Color.red);
         }
         
-        if (Physics.Raycast(transform.TransformPoint(Vector3.left / 2), Vector3.down, out leftHit, Mathf.Infinity))
+        if (Physics.Raycast(transform.TransformPoint(Vector3.left / 2), Vector3.down, out leftHit, transform.TransformPoint(Vector3.left / 2).y - transform.TransformPoint(Vector3.down / 2).y))
         {
             Debug.DrawLine(transform.TransformPoint(Vector3.left / 2), leftHit.point, Color.red);
         }
         
-        if (rb.velocity.y < 0 && Input.GetKeyDown(KeyCode.W) && Physics.Raycast(transform.position, Vector3.down, out rightHit, Mathf.Infinity) && Physics.Raycast(transform.TransformPoint(Vector3.left / 2), Vector3.down, out leftHit, Mathf.Infinity))
+        if (rb.velocity.y <= 0 && Physics.Raycast(transform.position, Vector3.down, out rightHit, transform.TransformPoint(Vector3.right / 2).y - transform.TransformPoint(Vector3.down / 2).y) && Physics.Raycast(transform.TransformPoint(Vector3.left / 2), Vector3.down, out leftHit, transform.TransformPoint(Vector3.left / 2).y - transform.TransformPoint(Vector3.down / 2).y))
         {
-            rb.AddForce(Vector2.up * (jumpSpeed * Time.deltaTime));
+            grounded = true;
+            
+            if (!grounded)
+                transform.position = new Vector3(transform.position.x, rightHit.point.y, transform.position.z);//This fixes the height of the object when falling onto the ground
+
+            if (grounded && Input.GetKeyDown(KeyCode.W))
+            {
+                rb.AddForce(Vector2.up * (jumpSpeed * Time.deltaTime));
+                grounded = false;
+            }
+        }
+        else
+        {
+            grounded = false;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(Vector2.left * (moveSpeed *  Time.deltaTime));
+            rb.AddForce(Vector2.left * (moveSpeed * Time.deltaTime));
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(Vector2.right * (moveSpeed *  Time.deltaTime));
+            rb.AddForce(Vector2.right * (moveSpeed * Time.deltaTime));
         }
     }
 }
